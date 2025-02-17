a6535b4bb01e92ae191a0b5d1f8523bb46c70f5a
Ildar Gazizov <vito.scalepta@gmail.com>
2025-02-17 18:53:56 +0500

    Update interceptors.ts

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -2,7 +2,8 @@ import axios, { type CreateAxiosDefaults } from 'axios'
 
 
 const options: CreateAxiosDefaults = {
-	baseURL: 'http://147.182.189.56:4200/api',
+	// baseURL: 'http://147.182.189.56:4200/api',
+	baseURL: '/api',
 	headers: {
 		'Content-Type': 'application/json'
 	},
